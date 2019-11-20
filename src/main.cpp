55633af0258621a2184f2a3298c18b719f8d25aa
Jonathan SIX <j.six@student.isartdigital.com>
2019-11-20 17:23:56 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "entity.hpp"
 #include "window.hpp"
 #include "renderer.hpp"
+#include "rasterizer.hpp"
+#include "vertex.hpp"
 
 
 using namespace std;
@@ -14,12 +16,18 @@ int main()
 {
 	Window win (1024, 700);
 	Renderer ren (win.get(), 1024, 700);
+	Rasterizer ras;
+
 	bool running = true;
 
 	while (running)
 	{
-		ren.clear ();
+		//ren.clear ();
 
+		Vertex v1 = {1.f, 1.f, 1.f};
+		Vertex v2 = {1.f, 10.f, 1.f};
+
+		ras.drawTriangle(ren.getDrawBuffer(), v1, v2, v1);
 
 		ren.swapBuffer ();
 	}
--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -1,5 +1,7 @@
 #include "rasterizer.hpp"
 
+
+
 void Rasterizer::drawLine()
 {
 }
